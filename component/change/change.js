d9328081e34f1c1f562ca6784b925ddf4861aba0
new_bird <2014229290>
2023-07-29 19:26:29 +0800

    组件化 组件引入 observe计算属性  wxs过滤

---
+++ component/change/change.js
@@ -0,0 +1,115 @@
+// component/change/change.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    // 小程序监听器
+    observers: {
+        "numb,count": function (num, count) {
+            this.setData({
+                sum: num + count
+            })
+        },
+        '_rgb.**': function (obj) {
+            this.setData({
+                defColor: `${obj.r}, ${obj.g}, ${obj.b}`
+            })
+        }
+    },
+    properties: {
+        //接受引用该组件的值 可以规定类型 类似父传子
+        // max:Number
+        // 完整格式
+        max: {
+            type: Number,
+            value: 10
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        count: 0,
+        numb: 0,
+        sum: 0,
+        _rgb: {
+            r: 0,
+            g: 0,
+            b: 0
+        },
+        defColor: "0,0,0"
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        addCount() {
+            if (this.data.count >= this.properties.max) return
+            this.setData({
+                count: this.data.count + 1
+            }),
+                this._showCount()
+        },
+        addCount2() {
+            if (this.data.count >= this.properties.max) return
+            this.setData({
+                numb: this.data.numb + 1
+            }),
+                this._showCount()
+        },
+        // 自定义属性
+        _showCount() {
+            wx.showToast({
+                title: 'count值为:' + this.data.count + this.data.numb,
+                icon: "none"
+            })
+        },
+        changeR() {
+            this.setData({
+                '_rgb.r': this.data._rgb.r + 5 > 255 ? 255 : this.data._rgb.r + 5
+            })
+        },
+        changeG() {
+            this.setData({
+                '_rgb.g': this.data._rgb.g + 5 > 255 ? 255 : this.data._rgb.g + 5
+            })
+        },
+        changeB() {
+            this.setData({
+                '_rgb.b': this.data._rgb.b + 5 > 255 ? 255 : this.data._rgb.b + 5
+            })
+        },
+        //自定义随机颜色
+        _randomColor() {
+            this.setData({
+                _rgb: {
+                    r: Math.floor(Math.random() * 256),
+                    g: Math.floor(Math.random() * 256),
+                    b: Math.floor(Math.random() * 256),
+                }
+            })
+        }
+    },
+    lifetimes: {
+        created() {
+            console.log("created执行");
+        },
+        attached() {
+            console.log("attached执行");
+        }
+    },
+    pageLifetimes: {
+        show() {
+            console.log("show");
+            this._randomColor()
+        },
+        hide() {
+            console.log("hide");
+        },
+        resize() {
+            console.log("resize");
+        }
+    }
+})
--- pages/test/test.js
+++ pages/test/test.js
@@ -1,46 +1,72 @@
 // pages/test/test.js
 Page({
-
+    //上拉加载
+    onPullDownRefresh() {
+        
+        // 可以修改数据等
+        wx.stopPullDownRefresh()
+    },
+    //下拉触底
+    onReachBottom:function(){
+        console.log("触发触底事件");
+    },
     /**
      * 页面的初始数据
      */
-    onLoad(options){
+    onLoad(options) {
+        // 接受参数
         console.log(options);
-        wx.request({
-          url: 'http://5990367be1e4470011c46fa8.mockapi.io/meng/user',
-          success(res){
-              console.log(res);
-            //   wx.showToast({
-            //     title: '登陆成功',
-            //     icon:"success",
-            //     duration:2000
-            //   })
-            wx.showModal({
-              title: '你好世界',
-              content: '这是登录提示',
-              complete: (res) => {
-                if (res.cancel) {
-                  console.log("用户取消了")
-                  wx.setStorage({
-                    key:"key",
-                    data:123456  
-                  })
+        this.setData({
+            query:options
+        })
+        //加载请求动画
+        wx.showLoading({
+            title: '数据加载中',
+        }),
+            wx.request({
+                url: 'http://5990367be1e4470011c46fa8.mockapi.io/meng/user',
+                success(res) {
+                    //   console.log(res);
+                    //   wx.showToast({
+                    //     title: '登陆成功',
+                    //     icon:"success",
+                    //     duration:2000
+                    //   })
+                    wx.showModal({
+                        title: '你好世界',
+                        content: '这是登录提示',
+                        complete: (res) => {
+                            if (res.cancel) {
+                                console.log("用户取消了")
+                                wx.setStorage({
+                                    key: "key",
+                                    data: 123456
+                                })
+                            }
+                            if (res.confirm) {
+                                console.log("用户确定了")
+                                //  wx.switchTab({
+                                //    url: '/pages/home/home',
+                                //  })
+                                wx.removeStorage({
+                                    key: "key",
+                                    success(res) {
+                                        console.log(res.data);
+                                    }
+                                })
+                            }
+                        }
+                    })
+                },
+                complete:()=>{
+                    wx.hideLoading()
                 }
-                if (res.confirm) {
-                  console.log("用户确定了")
-                //  wx.switchTab({
-                //    url: '/pages/home/home',
-                //  })
-                wx.getStorage({
-                    key:"key",
-                    success(res){
-                        console.log(res.data);
-                    }
-                })
-                }
-              }
             })
-          }
+    },
+    onReady(){
+        wx.setNavigationBarTitle({
+          title: this.data.query.name,
+          
         })
     },
     data: {
@@ -50,6 +76,7 @@ Page({
             name: "小然子",
             age: "18"
         },
+        query:{},
         user: [
             { id: 1, name: "小艾看到", age: 18 },
             { id: 2, name: "小艾到", age: 18 },
@@ -63,26 +90,27 @@ Page({
         ]
     },
     // 视图更新
-    changeview(){
-        this.setData({msg:"我是无敌小然子",
-        isshow:false,
-        ["obj.name"]:"小软子",
-        ["students[2].fs"]:90
-    })
+    changeview() {
+        this.setData({
+            msg: "我是无敌小然子",
+            isshow: false,
+            ["obj.name"]: "小软子",
+            ["students[2].fs"]: 90
+        })
     },
     click(event) {
-        console.log("事件出发了",event);
+        console.log("事件出发了", event);
     },
     handlefocus() {
         console.log("输入框获取焦点");
     },
-    getFs(e){
+    getFs(e) {
         console.log(e.target.dataset.fs);
     },
-    test1(){
+    test1() {
         console.log("父元素点击");
     },
-    test2(){
+    test2() {
         console.log("子元素点击");
     }
 })
